73261f124f9fe758db93700b7963c33f61d44275
ShepherdBoy <arraiamm@gmail.com>
2019-07-25 13:58:54 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -8,7 +8,7 @@ $conn = mysqli_connect($servername, $username, $password, $dbconnect);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-$conn->set_charset("utf8");
+//$conn->set_charset("utf8");
 echo "Connected successfully";
 #-----------------------------------------------------------#
 $sql = "SELECT category_id, category_name FROM market_category";
