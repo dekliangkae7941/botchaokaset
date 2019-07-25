e3dbb695284541a5bd3a83349052a1a476f6496d
ShepherdBoy <arraiamm@gmail.com>
2019-07-25 13:46:53 +0700

    Update config.php

--- admin/config.php
+++ admin/config.php
@@ -3,13 +3,14 @@ $servername = "localhostchatbot";
 $username = "dekliangkae";
 $password = "0967358315";
 $dbconnect = "chatbot_chaokaset";
-$conn = new mysqli($servername, $username, $password, $dbconnect);
+$conn = mysqli_connect($servername, $username, $password, $dbconnect);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 $conn->set_charset("utf8");
 echo "Connected successfully";
+
 
 ?>
 
