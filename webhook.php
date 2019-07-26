8bb74f87e88bc8cd3dfeb2bcdb9e26adebe44b60
ShepherdBoy <52777753+dekliangkae7941@users.noreply.github.com>
2019-07-26 10:00:00 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -6,6 +6,27 @@ require_once('./unirest-php-master/src/Unirest.php');
 $channelAccessToken = 'pZmLfAv73zYnio19mFJo2hudRTgr7y8FbMdAayR7VXep+rZyVt1NAAEL+ZcsjfbrA7VhuzmpTUfkkYIIkgjdfohQ5bf8XV781/5J/gIy5vzhQPrIgSXQ3Uj23DnEpFiCa+MC60K2WexRcqsdgTDQ6gdB04t89/1O/w1cDnyilFU='; 
 $channelSecret = 'ddfedb5ad9fad19c7c0bbe791cd28166';
 #-------------------------[Events]-------------------------#
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+$userId     = $client->parseEvents()[0]['source']['userId'];
+$groupId    = $client->parseEvents()[0]['source']['groupId'];
+$replyToken = $client->parseEvents()[0]['replyToken'];
+$timestamp  = $client->parseEvents()[0]['timestamp'];
+$type       = $client->parseEvents()[0]['type'];
+$message    = $client->parseEvents()[0]['message'];
+$profile    = $client->profil($userId);
+$repro = json_encode($profile);
+$messageid  = $client->parseEvents()[0]['message']['id'];
+$msg_type      = $client->parseEvents()[0]['message']['type'];
+
+$post_data      = $client->parseEvents()[0]['postback']['data'];
+
+$msg_file      = $client->parseEvents()[0]['message']['fileName'];
+$msg_message   = $client->parseEvents()[0]['message']['text'];
+$msg_title     = $client->parseEvents()[0]['message']['title'];
+$msg_address   = $client->parseEvents()[0]['message']['address'];
+$msg_latitude  = $client->parseEvents()[0]['message']['latitude'];
+$msg_longitude = $client->parseEvents()[0]['message']['longitude'];
+
 include('conectdb.php');
 date_default_timezone_set("Asia/Bangkok");
 $datef = date('Y-m-d');
