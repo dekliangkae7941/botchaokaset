3c1d0336181927e75e4980f4f17dea4c2e7e4698
ShepherdBoy <arraiamm@gmail.com>
2019-07-31 11:06:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+
   /*$LINEData = file_get_contents('php://input');
   $jsonData = json_decode($LINEData,true);
   $replyToken = $jsonData["events"][0]["replyToken"];
@@ -11,12 +11,38 @@
   $replyJson["messages"][0] = $replyText;
   $encodeJson = json_encode($replyJson);*/
 #--------------------------------------------------------------------------------------------------------------------#
-  $dbconn = pg_connect("host=ec2-107-22-211-248.compute-1.amazonaws.com dbname=dant72mtqngrqg user=zzeiglpdbgcsup password=357b5ef3838e36150679d259aeb37a2c9d2ec1dafb8ae5c90e7669d040874a9e");
+<!DOCTYPE html>
+<head>
+<title>Insert data to PostgreSQL with php - creating a simple web application</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<style>
+li {
+list-style: none;
+}
+</style>
+</head>
+<body>
+<h2>Enter data into book table</h2>
+<ul>
+<form name="insert" action="insert.php" method="POST" >
+<li>Book ID:</li><li><input type="text" name="bookid" /></li>
+<li>Book Name:</li><li><input type="text" name="book_name" /></li>
+<li>Price (USD):</li><li><input type="text" name="price" /></li>
+<li>Date of publication:</li><li><input type="text" name="dop" /></li>
+<li><input type="submit" /></li>
+</form>
+</ul>
+</body>
+</html>
+<?php
+$dbconn = pg_connect("host=ec2-107-22-211-248.compute-1.amazonaws.com dbname=dant72mtqngrqg user=zzeiglpdbgcsup password=357b5ef3838e36150679d259aeb37a2c9d2ec1dafb8ae5c90e7669d040874a9e");
   if (!$dbconn){
   echo "<center><h1>Doesn't work =(</h1></center>";
   }else
    echo "<center><h1>Good connection</h1></center>";
-
+   $query = "INSERT INTO book VALUES ('$_POST[bookid]','$_POST[book_name]',
+   '$_POST[price]','$_POST[dop]')";
+   $result = pg_query($query); 
 
 
 #--------------------------------------------------------------------------------------------------------------------#
