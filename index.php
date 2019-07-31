be9cf520ea663df35973c9ddec8836d6a7c94fb7
ShepherdBoy <52777753+dekliangkae7941@users.noreply.github.com>
2019-07-31 09:39:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   $text = $jsonData["events"][0]["message"]["text"];
   $timestamp = $jsonData["events"][0]["timestamp"];
 #--------------------------------------------------------------------------------------------------------------------#
-  $dbConnect = pg_connect("host=ec2-107-22-211-248.compute-1.amazonaws.com dbname=dant72mtqngrqg user=zzeiglpdbgcsup password=357b5ef3838e36150679d259aeb37a2c9d2ec1dafb8ae5c90e7669d040874a9e");
+  $dbconn = pg_connect("host=ec2-107-22-211-248.compute-1.amazonaws.com dbname=dant72mtqngrqg user=zzeiglpdbgcsup password=357b5ef3838e36150679d259aeb37a2c9d2ec1dafb8ae5c90e7669d040874a9e");
   if (!$dbconn){
   echo "<center><h1>Doesn't work =(</h1></center>";
   }else
