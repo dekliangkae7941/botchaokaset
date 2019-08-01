3b4bb2dce379996a334b6baa77bd195cb52a67c6
ShepherdBoy <arraiamm@gmail.com>
2019-08-01 14:03:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -27,6 +27,39 @@ if (!$dbconn){
 echo "<center><h1>Doesn't work =(</h1></center>";
 }else
  echo "<center><h1>Good connection</h1></center>";
+
+#--------------------------------------------------------------------------------------------------------------------#
+
+// Attempt select query execution
+$sql = "SELECT * FROM line_type WHERE category_id = 1";
+if($result = mysqli_query($dbconn, $sql)){
+    if(mysqli_num_rows($result) > 0){
+        echo "<table>";
+            echo "<tr>";
+                echo "<th>id</th>";
+                echo "<th>first_name</th>";
+                echo "<th>last_name</th>";
+                echo "<th>email</th>";
+            echo "</tr>";
+        while($row = mysqli_fetch_array($result)){
+            echo "<tr>";
+                echo "<td>" . $row['type_id'] . "</td>";
+                echo "<td>" . $row['type_name'] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+        // Free result set
+        mysqli_free_result($result);
+    } else{
+        echo "No records matching your query were found.";
+    }
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+ 
+// Close connection
+mysqli_close($link);
+
 #--------------------------------------------------------------------------------------------------------------------#
   /*$LINEData = file_get_contents('php://input');
   $jsonData = json_decode($LINEData,true);
