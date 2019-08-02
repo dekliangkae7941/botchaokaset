b9eee7902fd235de833f2584ccacdc956f4643f9
ShepherdBoy <arraiamm@gmail.com>
2019-08-02 16:50:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -485,25 +485,11 @@ else {
                 'messages' => array(
                     array(
                         'type' => 'text',
-                        'text' => $type_name.$type_name
+                        'text' => $type_name
                     )
                 )
             );
         }
-            $json_headers = array();
-            function pushMsg($json_headers,$arrayPostData){
-                $strUrl = "https://api.line.me/v2/bot/message/push";
-                $ch = curl_init();
-                curl_setopt($ch, CURLOPT_URL,$strUrl);
-                curl_setopt($ch, CURLOPT_HEADER, false);
-                curl_setopt($ch, CURLOPT_POST, true);
-                curl_setopt($ch, CURLOPT_HTTPHEADER, $json_headers);
-                curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-                $result = curl_exec($ch);
-                curl_close ($ch);
-            }
 
 
     }
