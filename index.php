2e0e64749b15baf0209c81f40143078431f46d12
ShepherdBoy <arraiamm@gmail.com>
2019-08-07 14:24:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -486,19 +486,19 @@ else {
         
         $datacount = 0;
         
-        //while($row = pg_fetch_array($result)){
+        while($row = pg_fetch_array($result)){
         //while($eventrow = pg_fetch_assoc($result)){
         //while($eventrow = $result->pg_fetch_assoc()){
             $datacount = $datacount + 1;
             $type_id = $row['type_id'];
             $type_name = $row['type_name'];
 
-            $arrayPostData['messages'][0]['contents']['body']['contents'][0]['type'] = "button";
-            $arrayPostData['messages'][0]['contents']['body']['contents'][0]['style'] = "secondary";
-            $arrayPostData['messages'][0]['contents']['body']['contents'][0]['action']['type'] = "message";
-            $arrayPostData['messages'][0]['contents']['body']['contents'][0]['action']['label'] = ") ";
-            $arrayPostData['messages'][0]['contents']['body']['contents'][0]['action']['text'] = "event,";
-        //}
+            $arrayPostData['messages'][0]['contents']['body']['contents'][$datacount]['type'] = "button";
+            $arrayPostData['messages'][0]['contents']['body']['contents'][$datacount]['style'] = "secondary";
+            $arrayPostData['messages'][0]['contents']['body']['contents'][$datacount]['action']['type'] = "message";
+            $arrayPostData['messages'][0]['contents']['body']['contents'][$datacount]['action']['label'] = "$datacount) $type_name";
+            $arrayPostData['messages'][0]['contents']['body']['contents'][$datacount]['action']['text'] = "event,$type_id";
+        }
         //pg_free_result($result);
         $arrayPostData['messages'][0]['contents']['footer']['type'] = "box";
         $arrayPostData['messages'][0]['contents']['footer']['layout'] = "vertical";
@@ -506,7 +506,7 @@ else {
         $arrayPostData['messages'][0]['contents']['footer']['contents'][0]['text'] = "Rwar";
         $arrayPostData['messages'][0]['contents']['footer']['contents'][0]['wrap'] = true;
         $arrayPostData['messages'][0]['contents']['footer']['contents'][0]['align'] = "center";
-        $arrayPostData['messages'][0]['contents']['styles']['header']['backgroundColor'] = true;
+        $arrayPostData['messages'][0]['contents']['styles']['header']['backgroundColor'] = "#f4ee42";
         replyMsg($arrayHeader,$arrayPostData);
 
 
