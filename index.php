ea946276333008ed3478b4d46eaab4746830a580
ShepherdBoy <arraiamm@gmail.com>
2019-08-07 14:39:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -493,7 +493,7 @@ else {
             //$type_id = $row['type_id'];
             //$type_name = $row['type_name'];
             $arrayPostData['messages'][0]['contents']['body']['contents'][0]['type'] = "button";
-            $arrayPostData['messages'][0]['contents']['body']['contents'][0]['style'] = "secondary";
+            $arrayPostData['messages'][0]['contents']['body']['contents'][0]['styles'] = "secondary";
             $arrayPostData['messages'][0]['contents']['body']['contents'][0]['action']['type'] = "message";
             $arrayPostData['messages'][0]['contents']['body']['contents'][0]['action']['label'] = ") 55555";
             $arrayPostData['messages'][0]['contents']['body']['contents'][0]['action']['text'] = "event,";
@@ -505,7 +505,7 @@ else {
         $arrayPostData['messages'][0]['contents']['footer']['contents'][0]['text'] = "Rwar";
         $arrayPostData['messages'][0]['contents']['footer']['contents'][0]['wrap'] = true;
         $arrayPostData['messages'][0]['contents']['footer']['contents'][0]['align'] = "center";
-        $arrayPostData['messages'][0]['contents']['style']['header']['backgroundColor'] = "#f4ee42";
+        $arrayPostData['messages'][0]['contents']['styles']['header']['backgroundColor'] = "#f4ee42";
         replyMsg($arrayHeader,$arrayPostData);
 
 
