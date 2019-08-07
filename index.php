9095bbc5902ca2b7147b5ffe9dda5966a11645ec
ShepherdBoy <arraiamm@gmail.com>
2019-08-07 09:47:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -108,6 +108,21 @@ if($messages == "สวัสดี"){
        pushMsg($arrayHeader,$arrayPostData);
     }
 }
+
+#-------------------------[Func]-------------------------#
+function pushMsg($arrayHeader,$arrayPostData){
+    $strUrl = "https://api.line.me/v2/bot/message/push";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,$strUrl);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $result = curl_exec($ch);
+    curl_close ($ch);
+}
 #-------------------------[Events]-------------------------#
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
