abb027e3c87c2b65eb536a475d9eab679b3a1db4
ShepherdBoy <arraiamm@gmail.com>
2019-08-09 16:24:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -555,11 +555,12 @@ else {
                 $arrayPostData['messages'][0]['contents']['footer']['contents'][0]['align'] = "center";
                 $arrayPostData['messages'][0]['contents']['styles']['header']['backgroundColor'] = "#f4ee42";
                 pushMsg($arrayHeader,$arrayPostData);
-                $command = $type_name;
+                
                         }
                         
             }
-            elseif ($command == $type_name) {
+            $command = $type_name;
+            if ($command == $type_name) {
                 $typename = $type_name;
             }
             $querytype = "SELECT line_subtype.type_id,line_type.type_id,line_type.type_name,line_subtype.subtype_name , line_subtype.subtype_id FROM line_subtype RIGHT JOIN line_type ON line_subtype.type_id = line_type.type_id
