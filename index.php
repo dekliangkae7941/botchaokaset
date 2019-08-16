b53b23f429ce606b7c345b591f89e5a7295062b5
ShepherdBoy <arraiamm@gmail.com>
2019-08-16 12:23:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -732,7 +732,7 @@ else {
                 $arrayPostData['messages'][0]['contents']['contents'][$datacountrowtype1]['body']['contents'][0]['contents'][1]['weight'] = "bold";
                 $arrayPostData['messages'][0]['contents']['contents'][$datacountrowtype1]['body']['contents'][0]['contents'][1]['wrap'] = true;
                 //$datacountrowtype += 1;
-                $querystype = "SELECT *FROM line_subtype_all WHERE subtype_id = '$subtype_id'";
+                $querystype = "SELECT COUNT(*) FROM line_subtype_all WHERE subtype_id = '$subtype_id' GROUP BY location_name";
                 /*"SELECT *FROM line_subtype
                 RIGHT JOIN line_subtype_all
                 ON line_subtype.subtype_id = line_subtype_all.subtype_id
