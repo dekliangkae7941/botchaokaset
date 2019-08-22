4f842e3e6d8e12ff7ec991ba4e617981ed14f1c8
ShepherdBoy <arraiamm@gmail.com>
2019-08-22 09:05:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1082,6 +1082,30 @@ else {
             }
         }       
     /////////////////////////
+    elseif($command == "ราคาน้ำมัน"){
+        $client = new SoapClient("http://www.pttplc.com/webservice/pttinfo.asmx?WSDL",
+		    	array(
+                    "trace"      => 1,		// enable trace to view what is happening
+                    "exceptions" => 0,		// disable exceptions
+                    "cache_wsdl" => 0) 		// disable any caching on the wsdl, encase you alter the wsdl server
+		           );
+                    $params = array(
+                        'Language' => "en",
+                        'DD' => date('d'),
+                        'MM' => date('m'),
+                        'YYYY' => date('Y')
+                    );
+		    $data = $client->GetOilPrice($params);
+            $ob = $data->GetOilPriceResult;
+            $xml = new SimpleXMLElement($ob);
+            // PRICE_DATE , PRODUCT ,PRICE
+            foreach ($xml  as  $key =>$val) {  
+                if($val->PRODUCT != ' '){
+                    echo $val->PRODUCT .'  '.$val->PRICE.' บาท<br>';
+                }
+            }
+    
+    }
     #ตัวอย่าง Message Type "Text + Sticker"
     /*elseif($command == "สวัสดี"){
         $arrayPostData['messages'][0]['type'] = "text";
