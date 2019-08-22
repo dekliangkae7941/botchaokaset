b6e372fa0753e339177735d0c375d9a580df908a
ShepherdBoy <arraiamm@gmail.com>
2019-08-22 11:23:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -54,27 +54,6 @@ if($result = pg_query($dbconn, $sql)){
 } else{
     echo "ERROR: Could not able to execute $sql. " . pg_result_error($link);
 }
-$client = new SoapClient("http://www.pttplc.com/webservice/pttinfo.asmx?WSDL",
-array(
-    "trace"      => 1,		// enable trace to view what is happening
-    "exceptions" => 0,		// disable exceptions
-    "cache_wsdl" => 0) 		// disable any caching on the wsdl, encase you alter the wsdl server
-   );
-    $params = array(
-        'Language' => "en",
-        'DD' => date('d'),
-        'MM' => date('m'),
-        'YYYY' => date('Y')
-    );
-$data = $client->GetOilPrice($params);
-$ob = $data->GetOilPriceResult;
-$xml = new SimpleXMLElement($ob);
-// PRICE_DATE , PRODUCT ,PRICE
-foreach ($xml  as  $key =>$val) {  
-if($val->PRODUCT != ' '){
-    echo $val->PRODUCT .'  '.$val->PRICE.' บาท<br>';
-}
-}
 // Close connection
 //dfb33833ca384effa6b7d26c0145ecab//APIKEYข่าว
 #--------------------------------------------------------------------------------------------------------------------#
