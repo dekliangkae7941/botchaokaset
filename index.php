e204ac69007a33935515a7f8243add5741daaa75
ShepherdBoy <arraiamm@gmail.com>
2019-08-22 09:57:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -22,6 +22,27 @@ list-style: none;
 </body>
 </html>
 <?php
+$client = new SoapClient("http://www.pttplc.com/webservice/pttinfo.asmx?WSDL",
+array(
+    "trace"      => 1,		// enable trace to view what is happening
+    "exceptions" => 0,		// disable exceptions
+    "cache_wsdl" => 0) 		// disable any caching on the wsdl, encase you alter the wsdl server
+   );
+    $params = array(
+        'Language' => "en",
+        'DD' => date('d'),
+        'MM' => date('m'),
+        'YYYY' => date('Y')
+    );
+$data = $client->GetOilPrice($params);
+$ob = $data->GetOilPriceResult;
+$xml = new SimpleXMLElement($ob);
+// PRICE_DATE , PRODUCT ,PRICE
+foreach ($xml  as  $key =>$val) {  
+if($val->PRODUCT != ' '){
+    echo $val->PRODUCT .'  '.$val->PRICE.' บาท<br>';
+}
+}
 $dbconn = pg_connect("host=ec2-107-22-211-248.compute-1.amazonaws.com dbname=dant72mtqngrqg user=zzeiglpdbgcsup password=357b5ef3838e36150679d259aeb37a2c9d2ec1dafb8ae5c90e7669d040874a9e");
 if (!$dbconn){
 echo "<center><h1>Doesn't work =(</h1></center>";
