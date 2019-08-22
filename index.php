57e441379db6975c2b510831f84f2208c31189cb
ShepherdBoy <arraiamm@gmail.com>
2019-08-22 17:14:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1127,20 +1127,7 @@ else {
         //$arrayPostData['messages'][1]['type'] = "sticker";
         //$arrayPostData['messages'][1]['packageId'] = "2";
         //$arrayPostData['messages'][1]['stickerId'] = "34";
-        $url = 'https://newsapi.org/v2/top-headlines?country=th&apiKey=dfb33833ca384effa6b7d26c0145ecab';
-        function get_contents($url){
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-            curl_setopt($ch, CURLOPT_USERAGENT,"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36");
-            curl_setopt($ch, CURLOPT_URL, $url);
-
-            $data = curl_exec($ch);
-            curl_close($ch);
-            return $data;
-        }
-        $contents = get_contents($url);
-        file_put_contents('news.json', $contents);
+        
         ///////////////////////////////////////
 #-------------------------[Include]-------------------------#
         $urinews1 = file_get_contents('./news/news.json');
