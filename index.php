58485720292fdd372a423246ff04a4dedfd10357
ShepherdBoy <arraiamm@gmail.com>
2019-08-23 11:52:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -721,7 +721,8 @@ elseif ($msg_type == 'location') {
 /////////////
 else {
     if($command == 'ข้าว'||$command == 'ข้าวโพด'||$command == 'ถั่วเหลือง'||$command == 'ถั่วเขียว'||$command == 'ลำไย'||$command == 'คะน้า'|| $command == 'ตะไคร้หอม'||$command == 'เห็ด'||$command == 'กาแฟ'||$command == 'มันสำปะหลัง'||$command == 'มะเขือเทศ'||$command == 'กำหนดเอง'){
-        $query = "INSERT INTO line_log (plat_name) VALUES ('$msg_message') WHERE userId = '$userId'";
+        $command = $plat_name;
+        $query = "INSERT INTO line_log (plat_name) VALUES ('$plat_name') WHERE userId = '$userId'";
         $result = pg_query($query);
         $text = "ขอบคุณสำหรับการเลือกชนิดการเพาะปลูกเพื่อรับแจ้งเตือน";
         $mreply = array(
