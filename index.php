ddf0f5a01f273a1d6026537bf7ced388fc53621b
ShepherdBoy <arraiamm@gmail.com>
2019-08-23 12:18:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -502,6 +502,32 @@ elseif ($type == 'follow') {
         )
         )
     );
+    if($command == 'ข้าว'||$command == 'ข้าวโพด'||$command == 'ถั่วเหลือง'||$command == 'ถั่วเขียว'||$command == 'ลำไย'||$command == 'คะน้า'|| $command == 'ตะไคร้หอม'||$command == 'เห็ด'||$command == 'กาแฟ'||$command == 'มันสำปะหลัง'||$command == 'มะเขือเทศ'||$command == 'กำหนดเอง'){
+        //$command = $plat_name;
+        $query = "INSERT INTO line_log (userid , displayName, plat_name)VALUES ('$userId','$displayName','$command')";
+        $result = pg_query($query);
+        $text = "ขอบคุณสำหรับการเลือกชนิดการเพาะปลูกเพื่อรับแจ้งเตือน";
+        $mreply = array(
+        'replyToken' => $replyToken,
+        'messages' => array(
+            array(
+                'type' => 'text',
+                'text' => $text
+                )
+            )
+        );
+    }else{
+        $text = "กรุณาเลือกชนิดการเพาะปลูกเพื่อรับแจ้งเตือน";
+        $mreply = array(
+        'replyToken' => $replyToken,
+        'messages' => array(
+            array(
+                'type' => 'text',
+                'text' => $text
+                )
+            )
+        );
+    }
     
 }
 /////////////
@@ -720,22 +746,8 @@ elseif ($msg_type == 'location') {
 }
 /////////////
 else {
-    if($command == 'ข้าว'||$command == 'ข้าวโพด'||$command == 'ถั่วเหลือง'||$command == 'ถั่วเขียว'||$command == 'ลำไย'||$command == 'คะน้า'|| $command == 'ตะไคร้หอม'||$command == 'เห็ด'||$command == 'กาแฟ'||$command == 'มันสำปะหลัง'||$command == 'มะเขือเทศ'||$command == 'กำหนดเอง'){
-        //$command = $plat_name;
-        $query = "INSERT INTO line_log (userid , displayName, plat_name)VALUES ('$userId','$displayName','$command')";
-        $result = pg_query($query);
-        $text = "ขอบคุณสำหรับการเลือกชนิดการเพาะปลูกเพื่อรับแจ้งเตือน";
-        $mreply = array(
-        'replyToken' => $replyToken,
-        'messages' => array(
-            array(
-                'type' => 'text',
-                'text' => $text
-                )
-            )
-        );
-    } 
-    elseif ($command == 'ราคาผัก') {
+    
+    if ($command == 'ราคาผัก') {
         $categoryid = 1;
         //$type_id = 0;
     }
