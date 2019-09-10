2221794da0552a8a3031fffd57c49b84246934db
ShepherdBoy <arraiamm@gmail.com>
2019-09-10 12:07:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -106,33 +106,6 @@ $arrayHeader[] = "Content-Type: application/json";
 $arrayHeader[] = "Authorization: Bearer {$channelAccessToken}";
 
 #------------------------------------------------------------------------#
-$client = new SoapClient("http://www.pttplc.com/webservice/pttinfo.asmx?WSDL",
- array(
-     "trace"      => 1,		// enable trace to view what is happening
-     "exceptions" => 0,		// disable exceptions
-     "cache_wsdl" => 0) 		// disable any caching on the wsdl, encase you alter the wsdl server
- );
-     $params = array(
-         'Language' => "en",
-         'DD' => date('d'),
-         'MM' => date('m'),
-         'YYYY' => date('Y')
-     );
-$data = $client->GetOilPrice($params);
-$ob = $data->GetOilPriceResult;
-$xml = new SimpleXMLElement($ob);
-// PRICE_DATE , PRODUCT ,PRICE
-/*while($row = pg_fetch_array($data)){
-  echo "<tr>";
-      echo "<td>" . $row['PRODUCT'] . "</td>";
-      echo "<td>" . $row['PRICE'] . "</td>";
-  echo "</tr>";
-}*/
-foreach ($xml  as  $key =>$val) {  
- if($val->PRODUCT != ''){
-     echo $val->PRODUCT .'  '.$val->PRICE.' บาท<br>';
- }
-}
 #-------------------------[Events]-------------------------#
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
