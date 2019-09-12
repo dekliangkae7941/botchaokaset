f8abdac68a761b1bf1a4a255e3399656c1a3473b
ShepherdBoy <arraiamm@gmail.com>
2019-09-12 18:41:15 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,11 +1,15 @@
 <?php
-    include "connectdb.php";
+$dbconn = pg_connect("host=ec2-107-22-211-248.compute-1.amazonaws.com dbname=dant72mtqngrqg user=zzeiglpdbgcsup password=357b5ef3838e36150679d259aeb37a2c9d2ec1dafb8ae5c90e7669d040874a9e");
+  if (!$dbconn){
+  echo "<center><h1>Doesn't work =(</h1></center>";
+  }else
+   echo "<center><h1>Good connection</h1></center>";
    //$query = "INSERT INTO book VALUES ('$_POST[bookid]','$_POST[book_name]',
    //'$_POST[price]')";
    //$result = pg_query($query);
-   $querylog = "INSERT INTO admin_log VALUES ($_POST[main_name]','$_POST[title]','$_POST[description]','$_POST[url_link]','$_POST[url_image]')";
+   $querylog = "INSERT INTO admin_log VALUES ('$_POST[main_name]','$_POST[title]','$_POST[description]','$_POST[url_link]','$_POST[url_image]')";
    $result = pg_query($querylog);
-    $query = "SELECT * FROM admin_log"; 
+      $query = "SELECT * FROM admin_log"; 
     //3.เก็บข้อมูลที่ query ออกมาไว้ในตัวแปร result . 
     $result = pg_query($dbconn, $query); 
     //4 . แสดงข้อมูลที่ query ออกมา โดยใช้ตารางในการจัดข้อมูล: 
