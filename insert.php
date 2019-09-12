6a7f4f778fb60cc6b9f681bafba0fe7b4a2a772b
ShepherdBoy <arraiamm@gmail.com>
2019-09-12 18:45:17 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -8,8 +8,18 @@ $dbconn = pg_connect("host=ec2-107-22-211-248.compute-1.amazonaws.com dbname=dan
    //'$_POST[price]')";
    //$result = pg_query($query);
    $querylog = "INSERT INTO admin_log VALUES ('$_POST[main_name]','$_POST[title]','$_POST[description]','$_POST[url_link]','$_POST[url_image]')";
-   $result = pg_query($querylog);
-      $query = "SELECT * FROM admin_log"; 
+   //$result = pg_query($querylog);
+    if ($result = pg_query($querylog)) {
+      echo "<center>บันทึกสำเร็จ</center>";
+      header('refresh: 2; url=');
+      exit(0);
+    } else {
+        echo "<center>ไม่สามารถบันทึกได้</center>";
+        header('refresh: 2; url=');
+        exit(0);
+    }
+
+    $query = "SELECT * FROM admin_log"; 
     //3.เก็บข้อมูลที่ query ออกมาไว้ในตัวแปร result . 
     $result = pg_query($dbconn, $query); 
     //4 . แสดงข้อมูลที่ query ออกมา โดยใช้ตารางในการจัดข้อมูล: 
