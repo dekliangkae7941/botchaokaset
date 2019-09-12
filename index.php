8617fc91c0a50b9a73992308ef8e7a2e8f2db3b9
ShepherdBoy <arraiamm@gmail.com>
2019-09-12 11:25:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -35,12 +35,7 @@ list-style: none;
 </body>
 </html>
 <?php
-
-$dbconn = pg_connect("host=ec2-107-22-211-248.compute-1.amazonaws.com dbname=dant72mtqngrqg user=zzeiglpdbgcsup password=357b5ef3838e36150679d259aeb37a2c9d2ec1dafb8ae5c90e7669d040874a9e");
-if (!$dbconn){
-echo "<center><h1>Doesn't work =(</h1></center>";
-}else
- echo "<center><h1>Good connection</h1></center>";
+include('connectdb.php');
  /////////////////////////////////////////////////////////////////
  
 #--------------------------------------------------------------------------------------------------------------------#
