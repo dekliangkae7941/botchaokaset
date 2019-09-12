9993a75cabdf073c5e3ffd27efb67ad3da60d673
ShepherdBoy <arraiamm@gmail.com>
2019-09-12 17:50:18 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -3,24 +3,18 @@
    //$query = "INSERT INTO book VALUES ('$_POST[bookid]','$_POST[book_name]',
    //'$_POST[price]')";
    //$result = pg_query($query);
-
    include "connectdb.php";
-
-   //$keyword = $_POST['keyword'];
-   //$intent = $_POST['intent'];
-   $querylog = "INSERT INTO admin_log VALUES ('','$_POST[main_name]','$_POST[title]','$_POST[description]','$_POST[url_link]','$_POST[url_image]')";
-   //$result = pg_query($querylog);
-
-   if ($result = pg_query($querylog)) {
+   
+   $sql = "INSERT INTO admin_log (main_id,main_name, title,description,url_link,url_image) VALUES ('','$_POST[main_name]','$_POST[title]','$_POST[description]','$_POST[url_link]','$_POST[url_image]')";
+   if ($dbconn->pg_query($sql) === TRUE) {
        echo "<center>บันทึกสำเร็จ</center>";
-       //header('refresh: 2; url=');
+       header('refresh: 2; url=/admin');
        exit(0);
    } else {
        echo "<center>ไม่สามารถบันทึกได้</center>";
-       //header('refresh: 2; url=');
+       header('refresh: 2; url=/admin');
        exit(0);
    }
-   
       /*$query = "SELECT * FROM admin_log"; 
     //3.เก็บข้อมูลที่ query ออกมาไว้ในตัวแปร result . 
     $result = pg_query($dbconn, $query); 
