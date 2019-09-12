c09aeea9d494521fd694d7c2fce51fd42920f279
ShepherdBoy <arraiamm@gmail.com>
2019-09-12 16:54:40 +0700

    Update config.php

--- admin/config.php
+++ admin/config.php
@@ -1,18 +1,9 @@
 <?php
-$servername = "localhostchatbot";
-$username = "dekliangkae";
-$password = "0967358315";
-$dbconnect = "chatbot_chaokaset";
-$conn = mysqli_connect($servername, $username, $password, $dbconnect);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-//$conn->set_charset("utf8");
-echo "Connected successfully";
-
-
-
+$dbconn = pg_connect("host=ec2-107-22-211-248.compute-1.amazonaws.com dbname=dant72mtqngrqg user=zzeiglpdbgcsup password=357b5ef3838e36150679d259aeb37a2c9d2ec1dafb8ae5c90e7669d040874a9e");
+if (!$dbconn){
+echo "<center><h1>Doesn't work =(</h1></center>";
+}else
+ echo "<center><h1>Good connection</h1></center>";
 ?>
 
 
