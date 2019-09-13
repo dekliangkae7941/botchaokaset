44454e08a49bc7e8d537fd732596ea2a4334deb3
ShepherdBoy <arraiamm@gmail.com>
2019-09-13 11:39:12 +0700

    Update delete.php

--- admin/delete.php
+++ admin/delete.php
@@ -7,11 +7,11 @@ $sql = "DELETE FROM data WHERE id=".$id;
 
 if ($conn->query($sql) === TRUE) {
     echo "<center>ลบสำเร็จ</center>";
-    header('refresh: 2; url=/admin');
+    header('refresh: 2; index=/admin');
     exit(0);
 } else {
     echo "<center>ไม่สามารถลบได้</center>";
-    header('refresh: 2; url=/admin');
+    header('refresh: 2; index=/admin');
     exit(0);
 }
 
