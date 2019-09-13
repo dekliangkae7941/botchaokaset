2354fab3f027dce4e2cfdf99d94d4fe3ca0ea2b9
ShepherdBoy <arraiamm@gmail.com>
2019-09-13 12:08:35 +0700

    Update insert.php

--- admin/insert.php
+++ admin/insert.php
@@ -4,14 +4,14 @@ include "config.php";
 $keyword = $_POST['keyword'];
 $intent = $_POST['intent'];
 
-$sql = "INSERT INTO data (id,keyword, intent) VALUES ('','".$keyword."', '".$intent."')";
-if ($conn->query($sql) === TRUE) {
+$sql = "INSERT INTO data_intent (id,keyword, intent) VALUES ('','".$keyword."', '".$intent."')";
+if ($result = pg_query($sql)) {
     echo "<center>บันทึกสำเร็จ</center>";
-    header('refresh: 2; url=/admin');
+    header('refresh: 2; admin/index.php');
     exit(0);
 } else {
     echo "<center>ไม่สามารถบันทึกได้</center>";
-    header('refresh: 2; url=/admin');
+    header('refresh: 2; admin/index.php');
     exit(0);
 }
 
