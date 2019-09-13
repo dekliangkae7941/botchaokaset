964b54d70a8ede22bd134c4605ac51ada3f7c333
ShepherdBoy <arraiamm@gmail.com>
2019-09-13 12:12:07 +0700

    Update delete.php

--- admin/delete.php
+++ admin/delete.php
@@ -5,13 +5,13 @@ $id = $_GET['id'];
 // echo $id;
 $sql = "DELETE FROM data_tent WHERE id=".$id;
 
-if ($result = pg_query($querylog)) {
+if ($result = pg_query($sql)) {
     echo "<center>ลบสำเร็จ</center>";
-    header('refresh: 2; admin/index.php');
+    header('refresh: 2; index.php');
     exit(0);
 } else {
     echo "<center>ไม่สามารถลบได้</center>";
-    header('refresh: 2; admin/index.php');
+    header('refresh: 2; index.php');
     exit(0);
 }
 
