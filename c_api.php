e2a29c67a72ceec44bb2da322dcd3e111af8ae7a
ShepherdBoy <arraiamm@gmail.com>
2019-09-17 13:59:51 +0700

    Update c_api.php

--- c_api.php
+++ c_api.php
@@ -6,10 +6,13 @@ $querylocation = "SELECT * FROM line_log WHERE userid = '$userId'";
         $userId = $rowlocation['userId'];
         $latitude = $rowlocation['latitude'];
         $longitude = $rowlocation['longitude'];
-$myObj->userId = $_POST['userId'];;
-$myObj->latitud = $_POST['latitud'];
-$myObj->longitude = $_POST['longitude'];
-$myJSON = json_encode($myObj);
-echo $myJSON;
+        if($userId == $_POST['userId']){
+            $myObj->userId = $_POST['userId'];
+            $myObj->latitud = $latitude;
+            $myObj->longitude = $longitude;
+            $myJSON = json_encode($myObj);
+            echo $myJSON;
+
+        }
 
 ?>
