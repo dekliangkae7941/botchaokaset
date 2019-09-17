5c5e221677b3a4d751b276dd336a8f80e747077a
ShepherdBoy <arraiamm@gmail.com>
2019-09-17 13:57:09 +0700

    Update c_api.php

--- c_api.php
+++ c_api.php
@@ -1,5 +1,11 @@
 <?php
 include "config.php";
+$querylocation = "SELECT * FROM line_log WHERE userid = '$userId'";
+        $resultlocation= pg_query($dbconn, $querylocation);
+        $rowlocation = pg_fetch_array($resultlocation);
+        $userId = $rowlocation['userId'];
+        $latitude = $rowlocation['latitude'];
+        $longitude = $rowlocation['longitude'];
 $myObj->userId = $_POST['userId'];;
 $myObj->latitud = $_POST['latitud'];
 $myObj->longitude = $_POST['longitude'];
