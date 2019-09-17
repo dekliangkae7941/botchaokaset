bc001504cc5c3f0e396d1eeb610890a65a5833e2
ShepherdBoy <arraiamm@gmail.com>
2019-09-17 14:32:41 +0700

    Update c_api.php

--- c_api.php
+++ c_api.php
@@ -1,6 +1,6 @@
 <?php
 include "connectdb.php";
-$querylocation = "SELECT * FROM line_log WHERE userid = 'Udac6e87952f7ba83e230875996a1107f'";
+        $querylocation = "SELECT * FROM line_log WHERE userid = 'Udac6e87952f7ba83e230875996a1107f'";
         $resultlocation= pg_query($dbconn, $querylocation);
         $rowlocation = pg_fetch_array($resultlocation);
         $userId = $rowlocation['userId'];
