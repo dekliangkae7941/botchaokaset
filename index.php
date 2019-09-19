008d328076b8cd4b99714989a344f6837ded59e9
ShepherdBoy <arraiamm@gmail.com>
2019-09-19 14:02:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -138,6 +138,7 @@ $querylog = "SELECT * FROM line_log WHERE userid = 'Udac6e87952f7ba83e230875996a
   $data = array('latitude' => $latitude, 'longitude' => $longitude,'limit' => $limit );
   $response = Unirest\Request::post("$uri",array(),$data);
   $json = json_decode($response->raw_body, true);
+  echo json_encode($json);
 die;
   $resultlo = $json['data']['list'][0]['location_name'];
   $resultpn = $json['data']['list'][0]['province_name'];
