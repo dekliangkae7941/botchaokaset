78059406dbbc75c669ed05260ce02cf8e5c481e7
ShepherdBoy <arraiamm@gmail.com>
2019-09-23 08:11:04 +0700

    ed

---
+++ edit.php
@@ -0,0 +1,49 @@
+<?php
+    include "connectdb.php";
+    $main_id = $_GET['main_id'];
+    $main_name = $_GET['main_name'];
+    $title = $_GET['title'];
+    $description = $_GET['description'];
+    $url_link = $_GET['url_link'];
+    $url_image = $_GET['url_image'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>หน้าสำหรับเพิ่มข้อมูล</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+<h2>เพิ่มข้อมูลข่าวสารและคลังความรู้</h2>
+  <form action="update.php" method="post">
+    <div class="form-group">
+      <label for="main_name">Main Name</label>
+      <input type="text" class="form-control" id="main_name"  name="main_name"><?php echo $main_name; ?>
+    </div>
+    <div class="form-group">
+      <label for="title">Title</label>
+      <input type="text" class="form-control" id="title"  name="title"><?php echo $title; ?>
+    </div>
+    <div class="form-group">
+      <label for="description">Description</label>
+      <input type="text" class="form-control" id="description"  name="description"><?php echo $description; ?>
+    </div>
+    <div class="form-group">
+      <label for="url_link">Url Link</label>
+      <input type="text" class="form-control" id="url_link"  name="url_link"><?php echo $url_link; ?>
+    </div>
+    <div class="form-group">
+      <label for="url_image">Url Image</label>
+      <input type="text" class="form-control" id="url_image"  name="url_image"><?php echo $url_image; ?>
+    </div>
+    <button type="submit" class="btn btn-default">บันทึการแก้ไข</button>
+  </form>
+  <br />
+  <br />
+  
---
+++ update.php
@@ -0,0 +1,20 @@
+<?php
+    include "connectdb.php";
+   //$query = "INSERT INTO book VALUES ('$_POST[bookid]','$_POST[book_name]',
+   //'$_POST[price]')";
+   //$result = pg_query($query);
+   $main_id = $_GET['main_id'];
+   $querylog = "UPDATE admin_log SET main_name = '$_POST[main_name]', title = '$_POST[title]', description = '$_POST[description]', 
+   url_link = '$_POST[url_link]', url_image = '$_POST[url_image]' WHERE main_id = '$main_id";
+   //$result = pg_query($querylog);
+    if ($result = pg_query($querylog)) {
+      echo "<center>แก้ไขสำเร็จ</center>";
+      header('refresh: 1; index.php');
+      exit(0);
+    } else {
+        echo "<center>ไม่สามารถแก้ไขได้</center>";
+        header('refresh: 2; index.php');
+        exit(0);
+    }
+    //5. close connection
+?>
