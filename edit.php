3bb0f7ffd6294ae457a4231896d203cd4cd9da5f
ShepherdBoy <arraiamm@gmail.com>
2019-09-23 08:30:56 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,14 +1,15 @@
 <?php
     include "connectdb.php";
     $main_id = $_GET['main_id'];
-    $main_name = $_GET['main_name'];
-    $title = $_GET['title'];
-    $description = $_GET['description'];
-    $url_link = $_GET['url_link'];
-    $url_image = $_GET['url_image'];
+    // $main_name = $_GET['main_name'];
+    // $title = $_GET['title'];
+    // $description = $_GET['description'];
+    // $url_link = $_GET['url_link'];
+    // $url_image = $_GET['url_image'];
     $query = "SELECT * FROM admin_log WHERE main_id = $main_id"; 
     $result = pg_query($dbconn, $query); 
     $row = pg_fetch_array($result);
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -24,26 +25,26 @@
 
 <div class="container">
 <h2>เพิ่มข้อมูลข่าวสารและคลังความรู้</h2>
-  <form action="update.php" method="post">
+  <form action="update.php?main_id=<?=$row['main_id']; ?>" method="post">
     <div class="form-group">
       <label for="main_name">Main Name</label>
-      <input type="text" class="form-control" id="main_name"  name="main_name" value="<?php $main_name; ?>">
+      <input type="text" class="form-control" id="main_name"  name="main_name" value="<?php $row['main_name']; ?>">
     </div>
     <div class="form-group">
       <label for="title">Title</label>
-      <input type="text" class="form-control" id="title"  name="title" value="<?php $title; ?>">
+      <input type="text" class="form-control" id="title"  name="title" value="<?php $row['title']; ?>">
     </div>
     <div class="form-group">
       <label for="description">Description</label>
-      <input type="text" class="form-control" id="description"  name="description" value="<?php $description; ?>
-    </div>">
+      <input type="text" class="form-control" id="description"  name="description" value="<?php $row['description']; ?>">
+    </div>
     <div class="form-group">
       <label for="url_link">Url Link</label>
-      <input type="text" class="form-control" id="url_link"  name="url_link" value="<?php $url_link; ?>">
+      <input type="text" class="form-control" id="url_link"  name="url_link" value="<?php $row['url_link']; ?>">
     </div>
     <div class="form-group">
       <label for="url_image">Url Image</label>
-      <input type="text" class="form-control" id="url_image"  name="url_image" value="<?php $url_image; ?>">
+      <input type="text" class="form-control" id="url_image"  name="url_image" value="<?php $row['url_image']; ?>">
     </div>
     <button type="submit" class="btn btn-default">บันทึการแก้ไข</button>
   </form>
