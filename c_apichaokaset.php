8416405711d4be51cb7ed04cad32aa04938080b2
ShepherdBoy <arraiamm@gmail.com>
2019-09-25 10:10:12 +0700

    api

---
+++ c_apichaokaset.php
@@ -0,0 +1,29 @@
+<?php
+class API
+{
+    public function index_api() 
+    { 
+     $this->load->model('m_api', 'mapi');
+     $mapi = $this->mapi;
+     
+     $post = $_POST;
+     $status_login = 0;
+     $a =$post['a'];
+     $s =$post['s'];
+     $d =$post['d'];
+     $f =$post['f'];
+     $g =$post['g'];
+     $h =$post['h'];
+     $mapi->aa = $a;
+     $mapi->ss = $s;
+     $mapi->dd = $d;
+     $mapi->ff = $f;
+     $mapi->gg = $g;
+     $mapi->hh = $h;
+     echo $mapi->aa;
+     //$respon =$mapi->insert_review();
+     //echo $respon;
+       
+    }
+}
+?>
