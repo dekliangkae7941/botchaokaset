dc8c25914d45fc1338a871027a7e2d1b376e85f8
ShepherdBoy <arraiamm@gmail.com>
2019-09-25 10:17:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -201,6 +201,33 @@ $arrayHeader[] = "Content-Type: application/json";
 $arrayHeader[] = "Authorization: Bearer {$channelAccessToken}";
 
 #------------------------------------------------------------------------#
+
+
+function index_api() 
+    { 
+     $this->load->model('m_api', 'mapi');
+     $mapi = $this->mapi;
+     
+     $post = $_POST;
+     //$status_login = 0;
+     $a =$post['a'];
+     $s =$post['s'];
+     $d =$post['d'];
+     $f =$post['f'];
+     $g =$post['g'];
+     $h =$post['h'];
+     $mapi->aa = $a;
+     $mapi->ss = $s;
+     $mapi->dd = $d;
+     $mapi->ff = $f;
+     $mapi->gg = $g;
+     $mapi->hh = $h;
+     echo $h;
+     echo "kijlkjjh";
+     //$respon =$mapi->insert_review();
+     //echo $respon;
+       
+    }
 // $querylog = "SELECT * FROM line_log WHERE userid = 'Udac6e87952f7ba83e230875996a1107f'";
 //             $resultlog = pg_query($dbconn, $querylog);
 //             $rowlog = pg_fetch_array($resultlog);
