842d0e1bc4fb05fadf5749eb213999267fb56c9d
ShepherdBoy <arraiamm@gmail.com>
2019-09-25 12:10:44 +0700

    Update c_apichaokaset.php

--- c_apichaokaset.php
+++ c_apichaokaset.php
@@ -1,15 +1,15 @@
 <?php
-     $post = $_POST;
-     //$status_login = 0;
-     $environment =$post['environment']; //สภาพแวดล้อม
-     $plant_type =$post['plant_type']; //ชนิดพืช
-     $growth_phase =$post['growth_phase']; //ระยะการเจริญเติบโต
-     $problem =$post['problem']; //ปัญหา
-     $possible_symptoms =$post['possible_symptoms']; //อาการที่อาจพบ
-     $prevention =$post['prevention']; //แนวทางป้องกัน
+    $post = $_POST;
+    //$status_login = 0;
+    $environment =$post['environment']; //สภาพแวดล้อม
+    $plant_type =$post['plant_type']; //ชนิดพืช
+    $growth_phase =$post['growth_phase']; //ระยะการเจริญเติบโต
+    $problem =$post['problem']; //ปัญหา
+    $possible_symptoms =$post['possible_symptoms']; //อาการที่อาจพบ
+    $prevention =$post['prevention']; //แนวทางป้องกัน
     $data = array('environment' => "$environment", 'plant_type' => "$plant_type", 'growth_phase' => "$growth_phase",
                   'problem' => "$problem", 'possible_symptoms' => "$possible_symptoms", 'prevention' => "$prevention" );
-        echo json_encode($data);
+    echo json_encode($data);
      //$respon =$mapi->insert_review();
      //echo $respon;
        
