d47af58fdef382628938971fec732f8932b29690
ShepherdBoy <arraiamm@gmail.com>
2019-09-26 11:11:35 +0700

    Update c_api_chaokaset.php

--- c_api_chaokaset.php
+++ c_api_chaokaset.php
@@ -38,13 +38,13 @@
     if(isset($environment) && $environment!=''){
         echo "success";
         echo $plant_type;
-        $queryp = "SELECT * FROM line_plant WHERE plan_type = '$plant_type' ";
+        $queryp = "SELECT * FROM line_plant WHERE plant_type = '$plant_type' ";
         $resultp = pg_query($dbconn, $queryp);
         $row = pg_fetch_array($resultp);
         //while($row = pg_fetch_array($resultp)){
-        $plan_category_name = $row['plan_category_name'];
-        echo $plan_category_name;
-        $query = "SELECT * FROM line_log WHERE userid = 'Udac6e87952f7ba83e230875996a1107f'";
+        $plant_category_name = $row['plant_category_name'];
+        echo $plant_category_name;
+        $query = "SELECT * FROM line_log WHERE plan_category = '$plant_category_name'";
             if($resultlog = pg_query($dbconn, $query)){
                 if(pg_num_rows($resultlog) > 0){
                     while($rowlog = pg_fetch_array($resultlog)){
@@ -52,7 +52,6 @@
                     $userid = $rowlog['userid'];
                     $plan_category = $rowlog['plan_category'];
                     $rowuserid = 0;
-                    
                     //$arrayPostData['replyToken'] = $replyToken;
                     $arrayPostData['to'][$rowuserid] = $userid;
                     $arrayPostData['messages'][0]['type'] = "flex";
