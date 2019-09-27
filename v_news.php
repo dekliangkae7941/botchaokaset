e897f6aa6d4fe2aa6dd24d2da24c80691546ac12
ShepherdBoy <arraiamm@gmail.com>
2019-09-27 12:08:49 +0700

    Update v_news.php

--- v_news.php
+++ v_news.php
@@ -1,6 +1,29 @@
 <?php
 include "connectdb.php";
 include "index.php";
+require_once('./include/line_class.php');
+require_once('./unirest-php-master/src/Unirest.php');
+#-------------------------[Token]-------------------------#
+$channelAccessToken = 'YhqOTnlfJE6/yjWpkPRNR03ryOXTb7R8QaOVBkVL1Q5zAEhV8xJaMKBgGoLRZcVfA7VhuzmpTUfkkYIIkgjdfohQ5bf8XV781/5J/gIy5vyxnO+4kUs2EpOJtHjNpnb9ED5kGu9OFa3G17TukVvILQdB04t89/1O/w1cDnyilFU='; 
+$channelSecret = '83255aed1b77104d01142b5542945438';
+$content = file_get_contents('php://input');
+$arrayJson = json_decode($content, true);
+$arrayHeader = array();
+$arrayHeader[] = "Content-Type: application/json";
+$arrayHeader[] = "Authorization: Bearer {$channelAccessToken}";
+function replyMsg($arrayHeader,$arrayPostData){
+    $strUrl = "https://api.line.me/v2/bot/message/reply";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,$strUrl);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
+    curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $result = curl_exec($ch);
+    curl_close ($ch);
+}
 $query = "SELECT * FROM admin_log";
     if($result = pg_query($dbconn, $query)){
         if(pg_num_rows($result) > 0){
