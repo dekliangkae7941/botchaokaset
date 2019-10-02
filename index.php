dd032f71ec2b1b958f5bbad02455ddf5a0d1ccfc
ShepherdBoy <arraiamm@gmail.com>
2019-10-02 12:41:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -78,6 +78,27 @@
 <?php
 
 #--------------------------------------------------------------------------------------------------------------------#
+$uriww = "https://chaokaset.openservice.in.th/index.php/doaservices/notifysent";
+            $response = Unirest\Request::get("$uriww");
+            $json = json_decode($response->raw_body, true);
+            $i = 0;
+
+            $datacountrowtype1 = 0;
+            $datacountrowtype2 = 0;
+            for($i=0;$i<=4;$i++){
+                //$datacountrowtype2 += 1;
+                //$datacountrowtype3 += 1;
+              $name = $json[$i]['name'];
+              $growing = $json[$i]['growing'];
+              $weather = $json[$i]['weather'];
+              $problem = $json[$i]['problem'];
+              $warning = $json[$i]['detail']['warning'];
+              $solution = $json[$i]['detail']['solution'];
+              $date_start = $json[$i]['detail']['date_start'];
+              $date_end = $json[$i]['detail']['date_end'];
+            echo $name;
+            }
+
 #------------------------------------------------------------------------------------------------------------#
 #-------------------------[Include]-------------------------#
 require_once('./include/line_class.php');
