1244372cfc317f627e6e263b76ade4d24c10eafd
ShepherdBoy <arraiamm@gmail.com>
2019-10-08 16:03:33 +0700

    dfgh

--- c_api_broadcast.php
+++ c_api_broadcast.php
@@ -1,8 +1,9 @@
 <?php
-    include("admin/connectdb.php");
+    
     #-------------------------[Include]-------------------------#
     require_once('./include/line_class.php');
     require_once('./unirest-php-master/src/Unirest.php');
+    include("admin/connectdb.php");
     #-------------------------[Token]-------------------------#
     $channelAccessToken = 'YhqOTnlfJE6/yjWpkPRNR03ryOXTb7R8QaOVBkVL1Q5zAEhV8xJaMKBgGoLRZcVfA7VhuzmpTUfkkYIIkgjdfohQ5bf8XV781/5J/gIy5vyxnO+4kUs2EpOJtHjNpnb9ED5kGu9OFa3G17TukVvILQdB04t89/1O/w1cDnyilFU='; 
     $channelSecret = '83255aed1b77104d01142b5542945438';
--- c_api_chaokaset.php
+++ c_api_chaokaset.php
@@ -4,8 +4,14 @@
     require_once('./include/line_class.php');
     require_once('./unirest-php-master/src/Unirest.php');
     include("admin/connectdb.php");
-    include("bot_header.php");
     #-------------------------[Token]-------------------------#
+    $channelAccessToken = 'YhqOTnlfJE6/yjWpkPRNR03ryOXTb7R8QaOVBkVL1Q5zAEhV8xJaMKBgGoLRZcVfA7VhuzmpTUfkkYIIkgjdfohQ5bf8XV781/5J/gIy5vyxnO+4kUs2EpOJtHjNpnb9ED5kGu9OFa3G17TukVvILQdB04t89/1O/w1cDnyilFU='; 
+    $channelSecret = '83255aed1b77104d01142b5542945438';
+    $content = file_get_contents('php://input');
+    $arrayJson = json_decode($content, true);
+    $arrayHeader = array();
+    $arrayHeader[] = "Content-Type: application/json";
+    $arrayHeader[] = "Authorization: Bearer {$channelAccessToken}";
     function pushMsg($arrayHeader,$arrayPostData){
         $strUrl = "https://api.line.me/v2/bot/message/multicast";
         $ch = curl_init();
