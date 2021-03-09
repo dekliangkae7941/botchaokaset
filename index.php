86f2c3beaf727c6a53270fb1cc759178a4ed593e
ShepherdBoy <52777753+dekliangkae7941@users.noreply.github.com>
2021-03-09 19:36:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,21 +4,23 @@ $txt1 = "Learn PHP";
 $txt2 = "W3Schools.com";
 $cars = array("Volvo", "BMW", "Toyota");
 
+$latitude = "14.2469023" ;
+$longitude = "100.6058911";
+    
 echo $txt1 . "<br>";
 echo "Study PHP at " . $txt2 . "<br>";
 echo "My car is a " . $cars[0];
 
 echo $resulta . "<br>";
 
-
-$uri = "https://api.openweathermap.org/data/2.5/weather?lat=14.2469023&lon=100.6058911&lang=th&units=metric&appid=bb32ab343bb6e3326f9e1bbd4e4f5d31";
-$response = Unirest\Request::get("$uri");
-    $json = json_decode($response->raw_body, true);
-    $resulta = $json['name'];
-    $resultb = $json['weather'][0]['main'];
-    $resultc = $json['weather'][0]['description'];
-    $resultd = $json['main']['temp'];
-    $resulte = $json['coord']['lon'];
+ $uri = "https://api.openweathermap.org/data/2.5/weather?lat=" . $latitude . "&lon=" . $longitude . "&lang=th&units=metric&appid=bb32ab343bb6e3326f9e1bbd4e4f5d31";
+            $response = Unirest\Request::get("$uri");
+            $json = json_decode($response->raw_body, true);
+            $resulta = $json['name'];
+            $resultb = $json['weather'][0]['main'];
+            $resultc = $json['weather'][0]['description'];
+            $resultd = $json['main']['temp'];
+            $resulte = $json['coord']['lon'];
 
 
 
